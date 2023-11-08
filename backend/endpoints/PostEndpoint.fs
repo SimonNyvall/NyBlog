1815e8c34a6b98e2e623c5c4ff79ba56c5172bdf
Simon Nyvall <simon.k.nyvall@proton.me>
2023-11-08 20:55:15 +0100

    Fix: warnings in endpoints

--- backend/endpoints/PostEndpoint.fs
+++ backend/endpoints/PostEndpoint.fs
@@ -6,8 +6,8 @@ open Backend.Services.PostService
 
 let usePostEndpoint (app: WebApplication): WebApplication =
   
-  app.MapGet("/api/posts", Func<string>(fun () -> getLatestPostContent() ))
+  app.MapGet("/api/posts", Func<string>(fun () -> getLatestPostContent() )) |> ignore
 
-  app.MapGet("/api/posts/{id}", Func<int, string>(fun id -> getPreviousPostContent(id) ))
+  app.MapGet("/api/posts/{id}", Func<int, string>(fun id -> getPreviousPostContent(id) )) |> ignore
 
   app
