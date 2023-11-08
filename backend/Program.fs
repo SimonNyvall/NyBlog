8bc3d86da8dcb4faf8558450b7393cb20e6c6405
Simon Nyvall <simon.k.nyvall@proton.me>
2023-11-08 21:44:18 +0100

    Remove: open moduls that are not needed

--- backend/Program.fs
+++ backend/Program.fs
@@ -1,18 +1,8 @@
 namespace backend
 #nowarn "20"
-open System
-open System.Collections.Generic
-open System.IO
-open System.Linq
-open System.Threading.Tasks
-open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
 open Backend.Endpoints.PostEndpoint
 
 [<AutoOpen>]
